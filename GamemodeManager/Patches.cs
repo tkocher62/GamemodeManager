f4be8a22d644569ecfe60e7a7c4c4a15f539030e
Cyanox <Infernogaming621@gmail.com>
2020-05-22 14:54:43 -0400

    Added common config overrides

---
+++ GamemodeManager/Patches.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace GamemodeManager
+{
+	class LaterJoinPatch
+	{
+		[HarmonyPatch(typeof(CharacterClassManager), "LaterJoinPossible")]
+		public static void Postfix(bool __result)
+		{
+			__result = __result == true ? Configs.isLaterJoin : false;
+		}
+	}
+}
